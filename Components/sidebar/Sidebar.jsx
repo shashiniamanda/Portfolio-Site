b23ada118a45e6eceadc99bbae5b20cf2de1d33e
Shashini Amanda <83871154+shashiniamanda@users.noreply.github.com>
2022-07-19 16:58:21 -0700

    Create Sidebar.jsx

---
+++ Components/sidebar/Sidebar.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import "./Sidebar.css";
+import Logo from "../../assets/logo.svg";
+
+const Sidebar = () => {
+    return (
+        <aside className='aside'>
+            <a href='#home' className='nav_logo'>
+                <img src={Logo} alt="logo" />
+            </a>
+            <nav className='nav'>
+                <div className='nav_menu'>
+                    <ul className='nav_list'>
+                        <li className='nav_item'>
+                            <a href="#home" className="nav_link">
+                               <i className="icon-home"></i>
+                            </a>
+                        </li>
+                        <li className='nav_item'>
+                            <a href="#about" className="nav_link">
+                            <i className="icon-user"></i>
+                            </a>
+                        </li>
+                        <li className='nav_item'>
+                            <a href="#services" className="nav_link">
+                                <i className="icon-briefcase"></i>
+                            </a>
+                        </li>
+                        <li className='nav_item'>
+                            <a href="#resume" className="nav_link">
+                                <i className="icon-graduation"></i>
+                            </a>
+                        </li>
+                        <li className='nav_item'>
+                            <a href="#blog" className='nav_link'>
+                                <i className="icon-note"></i>
+                            </a>
+                        </li>
+                        <li className='nav_item'>
+                            <a href="#contact" className='nav_link'>
+                                <i className="icon-bubble"></i>
+                            </a>
+                        </li>   
+                    </ul>
+                </div>
+            </nav>
+            <div className='nav_footer'>
+                <span className="copyright">&copy; 2022 - 2023.</span>
+            </div>
+        </aside>
+    )
+}
+
+export default Sidebar
