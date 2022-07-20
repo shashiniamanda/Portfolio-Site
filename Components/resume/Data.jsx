5e4c870b3db247283c4cfbd8a9a54cd41a576a20
Shashini Amanda <83871154+shashiniamanda@users.noreply.github.com>
2022-07-20 05:06:55 -0700

    Add files via upload

---
+++ Components/resume/Data.jsx
@@ -0,0 +1,56 @@
+
+const Data =[
+  {
+        id: 1,
+        category: "education",
+        icon: "icon-graduation",
+        year:"2019 - present",
+        title: "Academic Degree",
+        desc: "Currently I'm following my academic degree. ",
+  },
+  {
+        id: 2,
+        category: "education",
+        icon: "icon-graduation",
+        year:"2013 - 2017",
+        title: "Bachelor's Degree",
+        desc:"I completed my Bachelor's degree from IIT University.",
+  },
+  {
+        id: 3,
+        category: "education",
+        icon: "icon-graduation",
+        year:"2009 - 2013",
+        title: "Honours Degree",
+        desc:"I completed my Honours Degree at IIT University.",
+  },
+  {
+        id: 4,
+        category: "experience",
+        icon: "icon-briefcase",
+        year:"2019 - present",
+        title: "Web designer",
+        desc:"Currently I'm working as a web designer at SSL company.",
+  },
+  {
+        id: 5,
+        category: "experience",
+        icon: "icon-briefcase",
+        year:"2013 - 2017",
+        title: "Front-end Developer",
+        desc:"I have 4 years experience of working as a Front-end developer",
+  },
+  {
+        id: 6,
+        category: "experience",
+        icon: "icon-briefcase",
+        year:"2009 - 2013",
+        title: "Back-end Developer",
+        desc:"I have 4 years experience of working as a Back-end developer",
+  },
+];
+
+
+
+
+export default Data
